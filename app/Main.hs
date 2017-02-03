73d0106ebc2d5115fffaeed6add48d868bc299c4
Martin Galese <martin@galese.net>
2017-02-03 06:49:13 -0500

    nicer error handling

--- app/Main.hs
+++ app/Main.hs
@@ -7,6 +7,9 @@ import           Options
 import           Protolude
 import           System.IO               (BufferMode (NoBuffering),
                                           hSetBuffering, stdout)
+import Network.HTTP.Types.Status
+import Network.HTTP.Types
+import Network.HTTP.Client
 
 data PatentOptions = PatentOptions
   {
@@ -28,9 +31,13 @@ main :: IO ()
 main = runCommand $ \opts args -> do
     hSetBuffering stdout NoBuffering
     let parse = EPODOC.parseToEPODOC . convertString $ headDef "" args
+        onNotFoundError (StatusCodeException s _ _)
+          | (statusCode s) == 404 = Just ("Not Found" :: [Char])
+          | otherwise = Nothing
+        onNotFoundError _ = Nothing
     case parse of
       (Left err) -> do
         print err
-      (Right epodoc) -> do
+      (Right epodoc) -> handleJust onNotFoundError (putStrLn) $ do
         token <- EPOOPS.requestOAuthToken ((convertString . consumerKey) opts) ((convertString . secretKey) opts)
         void $ EPOOPS.downloadEPODDOC token epodoc (strict opts)
